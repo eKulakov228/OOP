a57339f0655f068169b91a155dd5d850f03fbd70
Шрек <55573385+eKulakov228@users.noreply.github.com>
2020-12-22 21:25:43 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A classA = new A();
+            Console.WriteLine("Значение свойства с: " + classA.c);
+            Console.ReadKey();
+        }
+    }
+
+    class A
+    {
+        private int a = 3;
+        private int b = 5;
+
+        public int c
+        {
+            get { return a *= (a + b); }
+        }
+    }
+}
